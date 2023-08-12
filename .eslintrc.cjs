67d030923a142b9e819c932e539ad2e08bf3b246
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-08-11 21:02:57 -0400

    Remove code that I don't need and install vite for building and eslint+prettier for formatting

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [
+    {
+      files: ['*.test.ts', '*.test.tsx', '*.test.jsx', '*.test.js'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+      env: { 'jest/globals': true }
+    }
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
