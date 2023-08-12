eb4cccdad8fbfbffcbe8aaa43cc95ab76c2c3ca6
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-08-12 07:15:20 -0400

    set up vite for building

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'react-auth-context',
+      fileName: (format) => `react-auth-context.${format}.js`,
+    }
+  },
   plugins: [react()],
 });
