f178c6b35ada8b757122d5b331f29c13b62b8414
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-08-14 16:39:15 -0400

    bump version and try to fix exports

---
+++ local_dev/App.tsx
@@ -0,0 +1,10 @@
+import Demo from './Demo';
+
+export default function App() {
+  return (
+    <div>
+        <p>Authentication components and hooks for React applications.</p>
+          <Demo />
+    </div>
+  );
+}
---
+++ local_dev/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './mock';
+
+const rootElem = document.getElementById('root');
+
+if (rootElem) {
+  const root = createRoot(rootElem);
+  root.render(<App />);
+}
