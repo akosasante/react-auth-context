55fba2df418259d537441d21f678cfff2b1938e0
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-10-26 12:52:51 -0400

    update auth provider unit tests to work + only test typescript files

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testMatch: ['**/?(*.)+(spec|test).(t|j)s?(x)'],
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
--- src/AuthProvider.test.tsx
+++ src/AuthProvider.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen } from '@testing-library/react';
+import {act, render, screen, waitFor} from '@testing-library/react';
 import { AuthStatus } from './AuthStatus';
 import { AuthProvider } from './AuthProvider';
 import axios from 'axios';
@@ -19,7 +19,7 @@ const renderAppWithStatusAndUser = () => {
     const { status, user } = useAuth<User>();
     return (
       <div>
-        <p>{status}</p>
+        <p data-testid="status">{status}</p>
         <p>{user?.user?.name}</p>
       </div>
     );
@@ -41,10 +41,11 @@ const renderAppWithStatus = () => {
   // Create a test component that consumes the Context
   const TestComponent = () => {
     const { status } = useAuth();
+    // console.log(status)
     return (
-      <div>
-        <p>{status}</p>
-      </div>
+      <section>
+        <p data-testid="status">{status}</p>
+      </section>
     );
   };
 
@@ -66,7 +67,7 @@ beforeEach(() => {
   axiosMock = new MockAdapter(axios);
 });
 
-test('renders the component when wrapped with Context', async () => {
+test('renders the component when wrapped with Context without crashing', async () => {
   axiosMock
     .onGet('/user')
     .reply(200, { users: [{ id: 1, name: 'John Smith' }] });
@@ -90,28 +91,27 @@ test('renders the component when wrapped with Context', async () => {
     );
   };
 
-  await act(async () => {
-    render(<App />);
+  render(<App />);
+
+  await waitFor(() => {
+    expect(screen).toBeDefined();
   });
-  expect(screen.getByText('HI')).toBeInTheDocument();
 });
 
 test('if no user is found on initial render, set status to not-logged-in', async () => {
   axiosMock.onGet('/user').reply(404, { message: 'User not found' });
 
-  await act(async () => {
-    renderAppWithStatus();
-  });
-  expect(screen.getByText(AuthStatus.NotLoggedIn)).toBeInTheDocument();
+  renderAppWithStatus();
+
+  expect(await screen.findByText(AuthStatus.NotLoggedIn)).toBeInTheDocument();
 });
 
 test('if user is found on initial render, set status to logged in', async () => {
   axiosMock.onGet('/user').reply(200, { user: { id: 1, name: 'John Smith' } });
 
-  await act(async () => {
-    renderAppWithStatus();
-  });
-  expect(screen.getByText(AuthStatus.LoggedIn)).toBeInTheDocument();
+  renderAppWithStatus();
+
+  expect(await screen.findByText(AuthStatus.LoggedIn)).toBeInTheDocument();
 });
 
 test('if a different getUser path is provided, use that instead', async () => {
@@ -138,10 +138,9 @@ test('if a different getUser path is provided, use that instead', async () => {
     );
   };
 
-  await act(async () => {
-    render(<App />);
-  });
-  expect(screen.getByText(AuthStatus.LoggedIn)).toBeInTheDocument();
+  render(<App />);
+
+  expect(await screen.findByText(AuthStatus.LoggedIn)).toBeInTheDocument();
   const request = axiosMock.history.get[0];
   expect(request.url).toBe('/showCurrentUser');
 });
@@ -169,9 +168,8 @@ test('if axios options are passed in, use them', async () => {
     );
   };
 
-  await act(async () => {
-    render(<App />);
-  });
+  render(<App />);
+
   expect(screen.getByText(AuthStatus.LoggedIn)).toBeInTheDocument();
   const request = axiosMock.history.get[0];
   expect(request.headers).toMatchObject({ 'My-Header': 'My-Value' });
@@ -181,9 +179,8 @@ test('if fetch user is successful, should be able to grab user name from context
   const user = { id: 1, name: 'John Smith' };
   axiosMock.onGet('/user').reply(200, { user });
 
-  await act(async () => {
-    renderAppWithStatusAndUser();
-  });
-  expect(screen.getByText(AuthStatus.LoggedIn)).toBeInTheDocument();
-  expect(screen.getByText(user.name)).toBeInTheDocument();
+  renderAppWithStatusAndUser();
+
+  expect(await screen.findByText(AuthStatus.LoggedIn)).toBeInTheDocument();
+  expect(await screen.findByText(user.name)).toBeInTheDocument();
 });
